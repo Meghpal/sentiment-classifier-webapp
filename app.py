86ba978b6e8eab4f7e72322d4674fc498e702b7e
Meghpal <meghpal.s@gmail.com>
2020-12-27 16:13:27 +0530

    template ready

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/result.html")
+def result():
+    return render_template("result.html")
+
+
+@app.route("/data.html")
+def data():
+    return render_template("data.html")
+
+
 if __name__ == "__main__":
     app.run(debug=False)
