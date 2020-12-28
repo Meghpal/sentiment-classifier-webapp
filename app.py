54a91c9d8fcb4dbe6dc9866408e06cf824c6f8a1
Meghpal <meghpal.s@gmail.com>
2020-12-28 22:38:28 +0530

    working logic

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+import os
+import pickle
+
+from flask import Flask, render_template, send_from_directory
+from tensorflow.keras.models import load_model
+from helpers.cleaner import review_cleaner
 
 app = Flask(__name__)
+
+vectorizer = pickle.load(open("model/vectorizer_tfidf.h5","rb"))
+model = pickle.load(open("model/model.h5","rb"))
 
 
 @app.route("/")
@@ -8,14 +16,22 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/result.html")
-def result():
-    return render_template("result.html")
+@app.route("/result.html/<text>")
+def result(text):
+    cleaned = review_cleaner(text)
+    review_bag = vectorizer.transform([cleaned]).toarray()
+    prediction = model.predict(review_bag)
+    return render_template("result.html", res="Negative sentiment" if prediction[0] == 0 else "Positive sentiment")
 
 
 @app.route("/data.html")
 def data():
     return render_template("data.html")
+
+
+@app.route('/favicon.ico')
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == "__main__":
---
+++ helpers/cleaner.py
@@ -0,0 +1,17 @@
+import bs4
+import re
+
+from nltk.corpus import stopwords
+
+
+def review_cleaner(review):
+    
+    review = bs4.BeautifulSoup(review,features='html.parser').text
+    emoticons = re.findall(r'(?::|;|=)(?:-)?(?:\)|\(|D|P)', review)
+    review = re.sub("[^a-zA-Z]", " ",review)
+    review = review.lower().split()
+    eng_stopwords = set(stopwords.words("english"))
+    review = [w for w in review if not w in eng_stopwords]
+    review = ' '.join(review + emoticons)
+
+    return(review)
