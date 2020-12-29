aae937caa3fcb9ed41a0c1fa78de2bf107ae601b
Meghpal <meghpal.s@gmail.com>
2020-12-29 14:25:56 +0530

    result through POST

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 import pickle
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from tensorflow.keras.models import load_model
 from helpers.cleaner import review_cleaner
 
 app = Flask(__name__)
 
-vectorizer = pickle.load(open("model/vectorizer_tfidf.h5","rb"))
-model = pickle.load(open("model/model.h5","rb"))
+vectorizer = pickle.load(open("model/vectorizer_tfidf.h5", "rb"))
+model = pickle.load(open("model/model.h5", "rb"))
 
 
 @app.route("/")
@@ -16,12 +16,16 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/result.html/<text>")
-def result(text):
-    cleaned = review_cleaner(text)
+@app.route("/result.html", methods=["POST"])
+def result():
+    cleaned = review_cleaner(request.form.get("review"))
     review_bag = vectorizer.transform([cleaned]).toarray()
     prediction = model.predict(review_bag)
-    return render_template("result.html", res="Negative sentiment" if prediction[0] == 0 else "Positive sentiment")
+
+    return render_template(
+        "result.html",
+        res="Negative sentiment" if prediction[0] == 0 else "Positive sentiment",
+    )
 
 
 @app.route("/data.html")
@@ -29,9 +33,13 @@ def data():
     return render_template("data.html")
 
 
-@app.route('/favicon.ico')
-def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
 if __name__ == "__main__":
--- helpers/cleaner.py
+++ helpers/cleaner.py
@@ -5,13 +5,13 @@ from nltk.corpus import stopwords
 
 
 def review_cleaner(review):
-    
-    review = bs4.BeautifulSoup(review,features='html.parser').text
-    emoticons = re.findall(r'(?::|;|=)(?:-)?(?:\)|\(|D|P)', review)
-    review = re.sub("[^a-zA-Z]", " ",review)
+
+    review = bs4.BeautifulSoup(review, features="html.parser").text
+    emoticons = re.findall(r"(?::|;|=)(?:-)?(?:\)|\(|D|P)", review)
+    review = re.sub("[^a-zA-Z]", " ", review)
     review = review.lower().split()
     eng_stopwords = set(stopwords.words("english"))
     review = [w for w in review if not w in eng_stopwords]
-    review = ' '.join(review + emoticons)
+    review = " ".join(review + emoticons)
 
-    return(review)
+    return review
